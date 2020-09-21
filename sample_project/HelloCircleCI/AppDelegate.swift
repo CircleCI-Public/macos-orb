a15748954b7b837bb0c2bc828a0dd8a54772026f
Kyle Tryon <Kyle.Tryon@outlook.com>
2020-09-21 10:11:54 -0400

    add sample project + tests

---
+++ sample_project/HelloCircleCI/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  HelloCircleCI
+//
+//  Created by Adam on 10/09/2020.
+//  Copyright © 2020 CircleCI. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
