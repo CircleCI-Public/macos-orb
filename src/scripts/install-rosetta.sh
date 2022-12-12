0044eb7068231367fc99627e19f04011f9be3077
Adam Hartley <BytesGuy@users.noreply.github.com>
2022-12-12 22:58:23 +0000

    [CPE-1524] Add install Rosetta command (#46)
    * Add install Rosetta command
    
    * Satisfy linter

---
+++ src/scripts/install-rosetta.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+if [ "$(uname -m)" = "arm64" ]; then
+    /usr/sbin/softwareupdate --install-rosetta --agree-to-license
+else
+    echo "Rosetta 2 can only be installed on Apple Silicon!"
+    exit 1
+fi
