a93996cb073eb85e2c2d98d73bc5e7131f57d5f4
Adam Hartley <git@ahartley.com>
2022-12-12 22:30:26 +0000

    Satisfy linter

--- src/scripts/install-rosetta.sh
+++ src/scripts/install-rosetta.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-if [ $(uname -m) = "arm64" ]; then
+if [ "$(uname -m)" = "arm64" ]; then
     /usr/sbin/softwareupdate --install-rosetta --agree-to-license
 else
     echo "Rosetta 2 can only be installed on Apple Silicon!"
