be5e0ee4751acc7a35f1f04b53854db2f943971f
Adam Hartley <BytesGuy@users.noreply.github.com>
2023-09-18 17:38:50 +0100

    Housekeeping: Update tests and fix vnc command (#63)
    * Bump versions and migrate to M1
    
    * Resource class typo
    
    * Fix rbenv test
    
    * Update devices
    
    * Add intel test (for chruby)
    
    * Possibly fix VNC command on Ventura
    
    * Escape quotes
    
    * Fix intel simulator test
    
    * Attempt to open the pref pane twice
    
    * This actually may be unnecessary
    
    * Simplify and test
    
    * System Prefs in macOS 12
    
    * Bump npm packages
    
    * Remove flakey intel test
    
    * Erroneous resource class
    
    * Include more information in VNC step
    
    * Bump simulator
    
    * Update wording

--- src/scripts/enable-vnc.sh
+++ src/scripts/enable-vnc.sh
@@ -4,14 +4,31 @@ printf '\nCreating the user account...'
 sudo /usr/sbin/sysadminctl -addUser vncuser -fullName "${ORB_VAL_USERNAME}" -password "${!ORB_ENV_PASSWORD}" -admin
 
 MAC_VER=$(sw_vers -productVersion | cut -d. -f1)
-if (( "$MAC_VER" >= 12 )); then
-  printf '\nmacOS12+ requires VNC to be enabled via System Preferences...'
-  printf '\nGranting automation permissions...'
-  EPOCH=$(($(date +'%s * 1000 + %-N / 1000000')))
-  TCC="replace into access (service,client,client_type,auth_value,auth_reason,auth_version,indirect_object_identifier,flags,last_modified) values (\"kTCCServiceAccessibility\",\"/usr/sbin/sshd\",1,2,3,1,\"UNUSED\",0,$EPOCH);"
-  sudo sqlite3 "/Library/Application Support/com.apple.TCC/TCC.db" "$TCC"
-  sudo sqlite3 "$HOME/Library/Application Support/com.apple.TCC/TCC.db" "$TCC"
 
+printf '\nGranting automation permissions...'
+EPOCH=$(($(date +'%s * 1000 + %-N / 1000000')))
+TCC="replace into access (service,client,client_type,auth_value,auth_reason,auth_version,indirect_object_identifier,flags,last_modified) values (\"kTCCServiceAccessibility\",\"/usr/sbin/sshd\",1,2,3,1,\"UNUSED\",0,$EPOCH);"
+sudo sqlite3 "/Library/Application Support/com.apple.TCC/TCC.db" "$TCC"
+sudo sqlite3 "$HOME/Library/Application Support/com.apple.TCC/TCC.db" "$TCC"
+
+if (( "$MAC_VER" >= 13 )); then
+  printf '\nOpening System Settings...'
+  # Attempt to open the pref pane twice as sometimes it will default to the update pane
+  open x-apple.systempreferences:com.apple.Sharing-Settings.extension
+  sleep 5
+  open x-apple.systempreferences:com.apple.Sharing-Settings.extension
+
+  printf '\nRunning AppleScript. Please wait ~20 seconds...'
+  osascript -e "tell application \"System Events\"
+    tell process \"System Settings\"
+      delay 5
+      click button 1 of group 1 of scroll area 1 of group 1 of group 1 of group 2 of splitter group 1 of group 1 of window \"Sharing\"
+      delay 5
+    end tell
+  end tell"
+
+  sudo killall "System Settings"
+else
   printf '\nOpening System Preferences...'
   open -b com.apple.systempreferences /System/Library/PreferencePanes/SharingPref.prefPane
 
@@ -21,17 +38,16 @@ if (( "$MAC_VER" >= 12 )); then
       delay 5
       click checkbox 1 of row 7 of table 1 of scroll area 1 of group 1 of window \"Sharing\"
       delay 5
-      click checkbox 1 of row 2 of table 1 of scroll area 1 of group 1 of window 1
-      delay 5
-      click radio button \"All users\" of radio group 1 of group 1 of window \"Sharing\"
     end tell
-  end tell
+  end tell"
 
-  repeat until application \"System Preferences\" is not running
-    tell application \"System Preferences\"
-      quit
-      delay 2
-    end tell
-  end repeat"
-  printf '\nDone!...'
+  sudo killall "System Preferences"
 fi
+
+printf '\nDone! To Access VNC (SSH jobs only), run the following command to forward the VNC port:'
+IP=$(ifconfig en0 | awk '/inet / {print $2}')
+printf '\nssh -p 54782 %s -L5901:localhost:5900 -N' "$IP"
+printf '\nThen point your VNC client to localhost:5901 and use username %s and' "$MAC_ORB_VNC_USERNAME"
+printf '\nthe password set in your MAC_ORB_VNC_PASSWORD project environment variable'
+printf '\n\nTo view the screen for the distiller user, we recommend using the Screen Sharing app'
+printf '\nincluded with macOS and choose the "Share The Display" option when logging in to VNC'
--- src/scripts/switch-ruby.sh
+++ src/scripts/switch-ruby.sh
@@ -1,6 +1,20 @@
 #!/usr/bin/env bash
 
-if ! [ -x "$(command -v rbenv)" ]; then
+if command -v rbenv >/dev/null 2>&1; then
+    version=$(rbenv versions --bare | grep "$ORB_VAL_RUBY_VERSION" | head -n 1)
+    if [ -z "$version" ]; then
+        printf "\nNo Rubies installed that match version %s\n" "$ORB_VAL_RUBY_VERSION"
+        printf "\nInstalled versions:\n"
+        rbenv versions
+        exit 1
+    fi
+
+    rbenv global "$version"
+    rbenv rehash
+
+    printf "\nSet Ruby version succesfully:\n"
+    rbenv versions
+else
     xcode_major=$(/usr/bin/xcodebuild -version | awk 'NR==1{print $2}' | cut -d. -f1)
     xcode_minor=$(/usr/bin/xcodebuild -version | awk 'NR==1{print $2}' | cut -d. -f2)
     ruby_ver="$ORB_VAL_RUBY_VERSION"
@@ -18,18 +32,4 @@ if ! [ -x "$(command -v rbenv)" ]; then
     else
         echo "chruby $ruby_version" >> ~/.bash_profile
     fi
-else
-    version=$(rbenv versions --bare | grep "$ORB_VAL_RUBY_VERSION" | head -n 1)
-    if [ -z "$version" ]; then
-        printf "\nNo Rubies installed that match version %s\n" "$ORB_VAL_RUBY_VERSION"
-        printf "\nInstalled versions:\n"
-        rbenv versions
-        exit 1
-    fi
-
-    rbenv global "$version"
-    rbenv rehash
-
-    printf "\nSet Ruby version succesfully:\n"
-    rbenv versions
 fi
